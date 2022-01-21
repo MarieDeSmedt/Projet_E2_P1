8c3ca3a7f79bda995385d30ae947bb2147265cba
MarieDeSmedt <desmedtmarie@outlook.com>
2022-01-21 16:09:16 +0100

    save

--- func2.py
+++ func2.py
@@ -5,29 +5,87 @@ import pickle
 
 def user_input_features(): 
     
-    year = st.sidebar.number_input('Année de construction', step=1)
-    Age = round(2022-year,0)
-    
-    LotArea = st.sidebar.number_input('Surface totale en m²')
+    YearBuilt = st.sidebar.number_input('Année de construction', step=1)
+    reno = st.sidebar.checkbox('Cochez si vous fait des rénovations')
+    if reno:
+        YearRemodAdd= st.sidebar.number_input('Date de rénovation', step=1) 
+    else:
+        YearRemodAdd=YearBuilt
 
-    GrLivArea = st.sidebar.number_input('Surface au sol en m²')
+    LotArea = st.sidebar.number_input('Surface totale en pi²')
+    GrLivArea = st.sidebar.number_input('Surface au sol en pi²')
+    TotalBsmtSF= st.sidebar.number_input('Surface du sous-sol en pi²')
+    FstFlrSF= st.sidebar.number_input('Surface du rez-de-chaussée en pi²')
+    SndFlrSF= st.sidebar.number_input('Surface du premier étage en pi²')    
+    LotFrontage = st.sidebar.number_input('Taille de la façade en pi²')
     
-    LotFrontage = st.sidebar.number_input('Taille de la façade en m²')
-    
-    GarageArea = st.sidebar.number_input('Taille du garage en m²')
-    
+    TotRmsAbvGrd= st.sidebar.number_input('Nombre de pièces hors sous-sol et hors salles de bain', step=1)
+    FullBath = st.sidebar.number_input('Nombre de salles de bains', step=1)
+    BedroomAbvGr = st.sidebar.number_input('Nombre de chambres', step=1)
+    KitchenAbvGr = st.sidebar.number_input('Nombre de cuisines', step=1)
+    Fireplaces = st.sidebar.number_input('Nombre de cheminées', step=1)
+
+    garage = st.sidebar.checkbox('Cochez si vous avez un garage')
+    if garage:
+        GarageArea = st.sidebar.number_input('Taille du garage en pi²')
+        GarageCars = st.sidebar.number_input('Capacité du garage', step=1)
+    else:
+        GarageArea = 0
+        GarageCars = 0
+
+    veranda = st.sidebar.checkbox('Cochez si vous avez une veranda')
+    if veranda:
+        ScreenPorch = st.sidebar.number_input('Taille de la veranda en pi²')
+    else:
+        ScreenPorch = 0
+   
+    Porch = st.sidebar.checkbox("Cochez si vous avez un porche d'entrée")
+    if Porch:
+        state = st.sidebar.radio("Est-il couvert",("oui","non"))
+        if state == "oui":
+            OpenPorchSF = st.sidebar.number_input("Taille du porche d'entrée en pi²")
+            EnclosedPorch = 0
+        else:
+            EnclosedPorch = st.sidebar.number_input("Taille du porche d'entrée en pi²")
+            OpenPorchSF = 0
+    else:
+        OpenPorchSF = 0
+        EnclosedPorch = 0
+   
+
+    terrasse = st.sidebar.checkbox('Cochez si vous avez une terrasse en bois')
+    if terrasse:
+        WoodDeckSF = st.sidebar.number_input('Taille de la terrasse en pi²')
+    else:
+        WoodDeckSF = 0
+
     Fence = st.sidebar.checkbox('Cochez cette case si votre terrain est cloturé')
-
     Pool = st.sidebar.checkbox('Cochez cette case si vous possédez une piscine')
 
-    return Age,GrLivArea,LotFrontage,LotArea,GarageArea,Fence,Pool
+    return ScreenPorch,OpenPorchSF,WoodDeckSF,EnclosedPorch,GarageCars,Fireplaces,YearBuilt,YearRemodAdd,LotArea,GrLivArea,TotalBsmtSF,FstFlrSF,SndFlrSF,LotFrontage,GarageArea,TotRmsAbvGrd,FullBath,BedroomAbvGr,KitchenAbvGr,Fence,Pool
+   
 
-def create_dataset(Age,GrLivArea,LotFrontage,LotArea,GarageArea,Fence,Pool):
-    data = {'Age': Age,
-        'GrLivArea': GrLivArea,
-        'LotFrontage': LotFrontage,
+def create_dataset(ScreenPorch,OpenPorchSF,WoodDeckSF,EnclosedPorch,GarageCars,Fireplaces,YearBuilt,YearRemodAdd,LotArea,GrLivArea,TotalBsmtSF,FstFlrSF,SndFlrSF,LotFrontage,GarageArea,TotRmsAbvGrd,FullBath,BedroomAbvGr,KitchenAbvGr,Fence,Pool):
+    data = {
+        'ScreenPorch':ScreenPorch,
+        'OpenPorchSF':OpenPorchSF,
+        'WoodDeckSF':WoodDeckSF,
+        'EnclosedPorch':EnclosedPorch,
+        'GarageCars':GarageCars,
+        'Fireplaces':Fireplaces,
+        'YearBuilt': YearBuilt,
+        'YearRemodAdd': YearRemodAdd,
         'LotArea': LotArea,
+        'GrLivArea': GrLivArea,
+        'TotalBsmtSF':TotalBsmtSF,
+        'FstFlrSF':FstFlrSF,
+        'SndFlrSF':SndFlrSF,
+        'LotFrontage': LotFrontage,
         'GarageArea': GarageArea,
+        'TotRmsAbvGrd':TotRmsAbvGrd,
+        'FullBath':FullBath,
+        'BedroomAbvGr':BedroomAbvGr,
+        'KitchenAbvGr':KitchenAbvGr,
         'Fence': Fence,
         'Pool' : Pool
         }
@@ -35,6 +93,6 @@ def create_dataset(Age,GrLivArea,LotFrontage,LotArea,GarageArea,Fence,Pool):
     return dataset
 
 def get_prediction(dataset):
-    loaded_model = pickle.load(open("finalized_model.sav", 'rb'))
+    loaded_model = pickle.load(open("last_model.sav", 'rb'))
     prediction = loaded_model.predict(dataset)
     return prediction
--- main2.py
+++ main2.py
@@ -27,18 +27,27 @@ prediction=0
 
 
 # creation du dataset du bien à évaluer, seulement si critères remplis
-Age,GrLivArea,LotFrontage,LotArea,GarageArea,Fence,Pool=user_input_features()
+ScreenPorch,OpenPorchSF,WoodDeckSF,EnclosedPorch,GarageCars,Fireplaces,YearBuilt,YearRemodAdd,LotArea,GrLivArea,TotalBsmtSF,FstFlrSF,SndFlrSF,LotFrontage,GarageArea,TotRmsAbvGrd,FullBath,BedroomAbvGr,KitchenAbvGr,Fence,Pool=user_input_features()
 
 
-if Age!=0 and GrLivArea!=0 and LotFrontage!=0 and LotArea!=0 and GarageArea!=0:
-    df = create_dataset(Age,GrLivArea,LotFrontage,LotArea,GarageArea,Fence,Pool)
+if YearBuilt!=0 and LotArea!=0 and TotRmsAbvGrd!=0 and FullBath!=0 :
+    df = create_dataset(ScreenPorch,OpenPorchSF,WoodDeckSF,EnclosedPorch,GarageCars,Fireplaces,YearBuilt,YearRemodAdd,LotArea,GrLivArea,TotalBsmtSF,FstFlrSF,SndFlrSF,LotFrontage,GarageArea,TotRmsAbvGrd,FullBath,BedroomAbvGr,KitchenAbvGr,Fence,Pool)
     features_ok = True
     # Apply Model to Make Prediction
     loaded_model = pickle.load(open("last_model.sav", 'rb'))
     prediction = loaded_model.predict(df)
     formated_prediction = '{:,}$'.format(int(prediction))
 else: 
-    error='veuillez remplir tous les critères'
+    if  YearBuilt == 0 :
+        error="Veuillez indiquer l'année de construction"
+    elif LotArea == 0 :
+        error="Veuillez indiquer la surface totale du bien"
+    elif TotRmsAbvGrd == 0 :
+        error="Veuillez indiquer le nombre de pièces"
+    elif FullBath == 0 :
+        error="Veuillez indiquer le nombre de salles de bain"
+    else:
+        error="error fatale"
     features_ok = False
 
 
@@ -49,17 +58,36 @@ if cols[2].button('Evaluez votre bien'):
     if features_ok :
         cols[1].balloons()
         cols[1].markdown('<p class="big-font">{}</p>'.format(formated_prediction), unsafe_allow_html=True)
-    else: cols[2].write(error)
+    else: cols[2].warning(error)
     
-    col = st.columns(2)
+    row1 = st.columns(2)
     if not Fence and features_ok:
-        df_with_fence = create_dataset(Age,GrLivArea,LotFrontage,LotArea,GarageArea,True,Pool)
+        df_with_fence = create_dataset(ScreenPorch,OpenPorchSF,WoodDeckSF,EnclosedPorch,GarageCars,Fireplaces,YearBuilt,YearRemodAdd,LotArea,GrLivArea,TotalBsmtSF,FstFlrSF,SndFlrSF,LotFrontage,GarageArea,TotRmsAbvGrd,FullBath,BedroomAbvGr,KitchenAbvGr,True,Pool)
         prediction_with_fence = get_prediction(df_with_fence)
         evol =int(prediction_with_fence)-int(prediction)
-        col[0].metric('Si votre terrain était cloturé votre bien vaudrait:',int(prediction_with_fence),evol)
+        row1[0].metric('Si votre terrain était cloturé votre bien vaudrait:',int(prediction_with_fence),evol)
 
     if not Pool and features_ok:
-        df_with_pool = create_dataset(Age,GrLivArea,LotFrontage,LotArea,GarageArea,Fence,True)
+        df_with_pool = create_dataset(ScreenPorch,OpenPorchSF,WoodDeckSF,EnclosedPorch,GarageCars,Fireplaces,YearBuilt,YearRemodAdd,LotArea,GrLivArea,TotalBsmtSF,FstFlrSF,SndFlrSF,LotFrontage,GarageArea,TotRmsAbvGrd,FullBath,BedroomAbvGr,KitchenAbvGr,Fence,True)
         prediction_with_pool = get_prediction(df_with_pool)
         evol =int(prediction_with_pool)-int(prediction)
-        col[1].metric('Si vous possédiez une piscine votre bien vaudrait:',int(prediction_with_pool),evol)
+        row1[1].metric('Si vous possédiez une piscine votre bien vaudrait:',int(prediction_with_pool),evol)
+    
+    row2 = st.columns(3)
+    if GarageArea == 0 and features_ok:
+        df_with_garage = create_dataset(ScreenPorch,OpenPorchSF,WoodDeckSF,EnclosedPorch,GarageCars,Fireplaces,YearBuilt,YearRemodAdd,LotArea,GrLivArea,TotalBsmtSF,FstFlrSF,SndFlrSF,LotFrontage,50,TotRmsAbvGrd,FullBath,BedroomAbvGr,KitchenAbvGr,Fence,Pool)
+        prediction_with_garage = get_prediction(df_with_garage)
+        evol =int(prediction_with_garage)-int(prediction)
+        row2[0].metric('Si vous possédiez un garage de 50pi² votre bien vaudrait:',int(prediction_with_garage),evol)
+
+    if ScreenPorch == 0 and features_ok:
+        df_with_veranda = create_dataset(50,OpenPorchSF,WoodDeckSF,EnclosedPorch,GarageCars,Fireplaces,YearBuilt,YearRemodAdd,LotArea,GrLivArea,TotalBsmtSF,FstFlrSF,SndFlrSF,LotFrontage,GarageArea,TotRmsAbvGrd,FullBath,BedroomAbvGr,KitchenAbvGr,Fence,Pool)
+        prediction_with_veranda = get_prediction(df_with_veranda)
+        evol =int(prediction_with_veranda)-int(prediction)
+        row2[1].metric('Si vous possédiez une veranda de 50pi² votre bien vaudrait:',int(prediction_with_veranda),evol)
+    
+    if WoodDeckSF == 0 and features_ok:
+        df_with_terrase = create_dataset(ScreenPorch,OpenPorchSF,20,EnclosedPorch,GarageCars,Fireplaces,YearBuilt,YearRemodAdd,LotArea,GrLivArea,TotalBsmtSF,FstFlrSF,SndFlrSF,LotFrontage,GarageArea,TotRmsAbvGrd,FullBath,BedroomAbvGr,KitchenAbvGr,Fence,Pool)
+        prediction_with_terrase = get_prediction(df_with_terrase)
+        evol =int(prediction_with_terrase)-int(prediction)
+        row2[2].metric('Si vous possédiez une terrase en bois de 20pi² votre bien vaudrait:',int(prediction_with_terrase),evol)
